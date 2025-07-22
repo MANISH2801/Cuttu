629065c2e7f2c4ff494659215eca07e3b70f9e92
MANISH2801 <manishvasishta80@gmail.com>
2025-07-22 18:00:36 +0530

    fix mini bugs

--- routes/chapters.js
+++ routes/chapters.js
@@ -8,7 +8,7 @@
 const express = require('express');
 const pool = require('../db');
 const router = express.Router();
-const { authenticate, authorize } = require('../middleware/auth');
+const { authenticate, authorize } = require('../middlewares/auth');
 
 
 
