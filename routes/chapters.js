145539bac9fdecc1aa5aa17e065a58c188f96674
MANISH2801 <manishvasishta80@gmail.com>
2025-07-22 17:32:05 +0530

    admin tools 5

--- routes/chapters.js
+++ routes/chapters.js
@@ -8,7 +8,7 @@
 const express = require('express');
 const pool = require('../db');
 const router = express.Router();
-const { authenticate, authorize } = require('../middleware/auth');
+const { authenticate } = require('../middleware/auth');
 
 
 // 🔐 Middleware to protect admin routes
